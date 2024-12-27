adf9dc1c503b7db1d2d1ceb9cb8143dedd1348c8
Yaba Ernesto <yabaernesto@gmail.com>
2024-12-27 21:07:39 +0100

    feat: search for gym

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,27 @@
+// biome-ignore lint/style/useImportType: <explanation>
+import { Gym } from '@prisma/client'
+import type { GymsRepository } from '@/repositories/gyms-repository'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return {
+      gyms,
+    }
+  }
+}
